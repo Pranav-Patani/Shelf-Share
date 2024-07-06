40f1e2f1f54a7d2fb534decc769a0bc7cb001e51
Pranav-Patani <pranav.patani@gmail.com>
2024-07-06 15:52:53 +0530

    fix: correct dev tools configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,10 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 
-const baseConfig = {
-  extends: 'eslint:recommended',
-  parserOptions: {
-    ecmaVersion: 6
-  },
-  env: {
-    node: true,
-    browser: true,
-    es6: true
-  }
-};
 
-const customConfig = [
+
+export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
 ];
 
-// Merge configurations
-const mergedConfig = {
-  ...baseConfig,
-  ...customConfig
-};
-
-export default mergedConfig;
